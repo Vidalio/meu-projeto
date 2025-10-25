dfa8fb63b4985c65d99404cdf93368f6cab08c49
Miguel Vidal e Bernardo Costa <miguelvidal1068@gmail.com>
2025-10-25 16:19:06 +0100

    Adicionar funcoes de soma e subtracao no calc.py

---
+++ calc.py
@@ -0,0 +1,12 @@
+
+# calc.py
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Soma:", somar(5, 3))
+    print("Subtração:", subtrair(5, 3))
+
